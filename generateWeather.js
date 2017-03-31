98b440fe054f9c5e2193b0c8aeff331a51d12e3f
oyaro06 <oyaro06@gmail.com>
2017-04-01 00:05:02 +0300

    Add some content

--- generateWeather.js
+++ generateWeather.js
@@ -1,14 +1,13 @@
 // genPages.js
 // by oyaro06
 
-function getWeather(parent)
+function getData(parent)
 {
         var URL = "http://api.openweathermap.org/data/2.5/weather?id=";
         var API_KEY = "04d92298c1985b8c8feb3dafc7defae9";
         //var KEY = "d75da5829ee2244d5a25bbb10c1a7318";
         var ID = "707471"
-        var FULL_URL = URL + ID + "&APPID=" + API_KEY;
-
+        var FULL_URL = URL + ID + "&APPID=" + API_KEY; 
         var XHR = new XMLHttpRequest();
         XHR.open("GET", FULL_URL, true);
         XHR.send();
@@ -21,15 +20,28 @@ function getWeather(parent)
             var data = JSON.parse(XHR.responseText);
 
             var div = document.createElement("div");
-            div.innerHTML = "<span>Temperature: " + (data.main.temp - 273.15) + "<sup>o</sup>C</span>";
+            div.innerHTML = "<span>Temperature: " + (data.main.temp - 273.15).toFixed(1) + "<sup>o</sup>C</span>";
+            div.setAttribute("id", "temp");
             parent.appendChild(div);
 
             var div = document.createElement("div");
             div.innerHTML = "<span>Humidity: " + data.main.humidity + "%</span>";
+            div.setAttribute("id", "humidity");
             parent.appendChild(div);
         }
             
     };
+
+    return;
+}
+
+/*********************************************************************************/
+
+function generatePage(parent)
+{
+    var div = document.createElement("div");
+    div.setAttribute("id", "weather");
+    parent.appendChild(div);
 
     return;
 }
