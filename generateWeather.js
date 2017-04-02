10a5681e4f84f9e1262e8816b1a992c456351029
oyaro <oyaro@Empty.oyaro>
2017-04-02 20:52:20 +0300

    Release

--- generateWeather.js
+++ generateWeather.js
@@ -3,104 +3,39 @@
 
 function getData(parent, city)
 {
+        console.log(parent)
+        console.log(city)
+    
         var URL = "http://api.openweathermap.org/data/2.5/weather?id=";
         var API_KEY = "04d92298c1985b8c8feb3dafc7defae9";
+        var FULL_URL = URL + city + "&APPID=" + API_KEY; var XHR = new XMLHttpRequest();
 
-            var FULL_URL = URL + city + "&APPID=" + API_KEY; 
-            var XHR = new XMLHttpRequest();
+        console.log(FULL_URL);
 
-            console.log(FULL_URL);
+        XHR.open("GET", FULL_URL, true);
+        XHR.send();
 
-            XHR.open("GET", FULL_URL, true);
-            XHR.send();
-
-            XHR.onreadystatechange = function() 
+        XHR.onreadystatechange = function() 
+        {
+            if ((XHR.readyState === 4) && (XHR.status === 200)) 
             {
-                if ((XHR.readyState === 4) && (XHR.status === 200)) 
-                {
-                    var data = JSON.parse(XHR.responseText);
+                var data = JSON.parse(XHR.responseText);
 
-                    var div = document.createElement("div");
-                    div.innerHTML = "<span class=\"name\">" + data.name + "</span>";
-                    parent.appendChild(div);
+                var div = document.createElement("div");
+                div.innerHTML = "<span class=\"name\">" + data.name + "</span>";
+                parent.appendChild(div);
 
-                    var div = document.createElement("div");
-                    div.innerHTML = "<span class=\"temp\">Температура: " + (data.main.temp - 273.15).toFixed(1) + "<sup>o</sup>C</span>";
-                    parent.appendChild(div);
+                var div = document.createElement("div");
+                div.innerHTML = "<span class=\"temp\">Температура: " + (data.main.temp - 273.15).toFixed(1) + "<sup>o</sup>C</span>";
+                parent.appendChild(div);
 
-                    var div = document.createElement("div");
-                    div.innerHTML = "<span class=\"humidity\">Вологість: " + data.main.humidity + "%</span>";
-                    parent.appendChild(div);
-                }
-            };
-
-    return;
-}
-
-/*********************************************************************************/
-
-function generatePage(parent)
-{
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather1");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather2");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather3");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather4");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather5");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather6");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather7");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather8");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather9");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather10");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather11");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather12");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather13");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather14");
-    parent.appendChild(div);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "weather15");
-    parent.appendChild(div);
-
+                var div = document.createElement("div");
+                div.innerHTML = "<span class=\"humidity\">Вологість: " + data.main.humidity + "%</span>";
+                parent.appendChild(div);
+            }
+        };
+        
+        console.log("********************************************************");
 
     return;
 }
